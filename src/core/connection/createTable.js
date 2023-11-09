ac46f39493f3ea09b030d315585d08dd10504107
YaelFuentes <Yaelfuentes32@gmail.com>
2023-11-09 11:56:30 -0300

    login session knex database

---
+++ src/core/connection/createTable.js
@@ -0,0 +1,21 @@
+const { db } = require('./databaseService.js')
+
+async function createTable() {
+  try {
+    //Tabla usuarios
+    const usersTable = await db.schema.hasTable('users');
+    if (!usersTable) {
+      await db.schema.createTable('users', (table) => {
+        table.increments('id').primary().notNullable();
+        table.string('username')
+        table.string('password')
+      })
+      console.log(usersTable);
+    }
+  } catch (e) {
+    console.log(`La tabla que esta intentando crear ya se encuentra en ${process.env.DB_HOST}`);
+  }
+}
+
+// Call the createTable function
+createTable()
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,18 @@
-import '@/styles/globals.css'
+import '../styles/globals.css'
+import { SWRConfig } from "swr";
+import fetch from "../lib/fetchJson";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  return <SWRConfig
+    value={{
+      fetcher: fetch,
+      onError: (err) => {
+        console.error(err);
+      },
+    }}
+  >
+    <Component {...pageProps} />
+  </SWRConfig>;
 }
+
+export default MyApp
