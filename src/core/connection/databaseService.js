7fc3307f0c1a1ef0734e5525bf4be4d3a32bfe09
YaelFuentes <Yaelfuentes32@gmail.com>
2024-05-22 14:20:54 -0300

    testing fixed

--- src/core/connection/databaseService.js
+++ src/core/connection/databaseService.js
@@ -1,16 +1,21 @@
 const knex = require('knex');
 const dotenv = require('dotenv');
 dotenv.config();
-const db = knex({
-  client: 'mysql',
-  connection: {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB,
-  }
-});
+const createDbConnection = () => {
+  return knex({
+    client: 'mysql2',
+    connection: {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB,
+    },
+    acquireConnectionTimeout: 10000,
+  });
+};
+
+let db = createDbConnection();
 
 const databaseServiceFactory = () => {
   const TABLE = 'users';
